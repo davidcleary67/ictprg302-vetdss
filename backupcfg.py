499bed05326f330b867ecc7c20a7d44167701759
David Cleary <dcleary@sunitafe.edu.au>
2023-09-08 04:48:36 +0000

    Started backup.py

---
+++ backupcfg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+jobs = {"job700" : "/home/ec2-user/environment/ictprg302-vetdss/test/file1",
+        "job1"   : "/home/ec2-user/environment/ictprg302-vetdss/test/dir1"}
+        
+backupDir = "/home/ec2-user/environment/ictprg302-vetdss/backups"
+
+backupLog = "/home/ec2-user/environment/ictprg302-vetdss/backup.log"
+
+email = {}
---
+++ work.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import math
+
+n = int(input("Enter a number: "))
+
+for t in range(1, n + 1):
+    if True:
+    print("The number is " + str(t) + " its square is " + str(int(math.pow(t, 2))) + " and its cube is " + str(int(math.pow(t, 3))) + ".")
+    
+    
+
