f302d5c48985f0c9b97ba26f06b61e2bb8c13b70
David Cleary <dcleary@sunitafe.edu.au>
2023-09-20 03:52:14 +0000

    20 Sep

--- backup.py
+++ backup.py
@@ -4,35 +4,75 @@ import sys
 import os
 import pathlib
 import shutil
-from backupcfg import jobs, backupDir
+import smtplib
+from backupcfg import jobs, backupDir, backupLog, smtp
 from datetime import datetime
 
-def main():
-    argCount = len(sys.argv)
-    if not argCount == 2:
-        print("ERROR: job not specified")
-    else:
-        job = sys.argv[1]
-        if not job in jobs:
-            print(f"ERROR: job {job} does not exist")
+def writeLogMessage(logMessage, dateTimeStamp, isSuccess):
+    try:
+        file = open(backupLog, "a")
+        
+        if isSuccess:
+            file.write(f"SUCCESS {dateTimeStamp} {logMessage}\n")
         else:
-            source = jobs[job]
-            if not os.path.exists(source):
-                print(f"ERROR: source {source} does not exist")
+            file.write(f"FAILURE {dateTimeStamp} {logMessage}\n")
+            
+        file.close()
+        
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    except IOError:
+        print("ERROR: File is not accessible.")
+    
+def errorHandler(errorMessage, dateTimeStamp):
+    print(errorMessage)
+    writeLogMessage(errorMessage, dateTimeStamp, False)
+    sendEmail(errorMessage)
+
+# append all error messages to email and send
+def sendEmail(message):
+
+    email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Backup Error\n\n' + message + '\n'
+
+    # connect to email server and send email
+    try:
+        smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.ehlo()
+        smtp_server.login(smtp["user"], smtp["password"])
+        smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)
+        smtp_server.close()
+    except Exception as e:
+        print("ERROR: An error occurred.")    
+
+def main():
+    dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    argCount = len(sys.argv)
+    if argCount < 2:
+        errorHandler("ERROR: job not specified", dateTimeStamp)
+    else:
+        for job in sys.argv[1:]:
+            if not job in jobs:
+                errorHandler(f"ERROR: job {job} does not exist", dateTimeStamp)
             else:
-                destination = backupDir
-                if not os.path.exists(destination):
-                    print(f"ERROR: destination {destination} does not exist")
+                source = jobs[job]
+                if not os.path.exists(source):
+                    errorHandler(f"ERROR: source {source} does not exist", dateTimeStamp)
                 else:
-                    dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-                    srcPath = pathlib.PurePath(source)
-                    dstLoc = destination + "/" + srcPath.name + "-" + dateTimeStamp
-                    if pathlib.Path(source).is_dir():
-                        shutil.copytree(source, dstLoc)
+                    destination = backupDir
+                    if not os.path.exists(destination):
+                        errorHandler(f"ERROR: destination {destination} does not exist", dateTimeStamp)
                     else:
-                        shutil.copy2(source, dstLoc)
-                    pass
-                    
+                        srcPath = pathlib.PurePath(source)
+                        dstLoc = destination + "/" + srcPath.name + "-" + dateTimeStamp
+                        if pathlib.Path(source).is_dir():
+                            shutil.copytree(source, dstLoc)
+                        else:
+                            shutil.copy2(source, dstLoc)
+                            
+                        writeLogMessage(f"Backed up {source} to {dstLoc}", dateTimeStamp, True)
+                        
 
 if __name__ == "__main__":
     main()
--- backupcfg.py
+++ backupcfg.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
-jobs = {"job700" : "/home/ec2-user/environment/ictprg302-vetdss/test/file1",
-        "job1"   : "/home/ec2-user/environment/ictprg302-vetdss/test/dir1"}
+jobs = {"job1" : "/home/ec2-user/environment/ictprg302-vetdss/test/file1",
+        "job2" : "/home/ec2-user/environment/ictprg302-vetdss/test/dir1",
+        "job3" : "/home/ec2-user/environment/ictprg302-vetdss/test/dir2"}
+
         
 backupDir = "/home/ec2-user/environment/ictprg302-vetdss/backups"
 
 backupLog = "/home/ec2-user/environment/ictprg302-vetdss/backup.log"
 
-email = {}
+smtp = {"sender": "dcleary@sunitafe.edu.au",
+        "recipient": "davidcgcleary@gmail.com",
+        "server": "smtp.gmail.com",
+        "port": 587,
+        "user": "davidcgcleary@gmail.com", # need to specify a gmail email address with an app password setup
+        "password": "lemkllikuhmpdoog"}   # need a gmail app password     
