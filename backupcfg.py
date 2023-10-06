cd0e5c2951757e89db9e2b3d4bc32914a7680d4f
David Cleary <dcleary@sunitafe.edu.au>
2023-10-06 03:10:54 +0000

    6/10/2023

--- backupcfg.py
+++ backupcfg.py
@@ -9,9 +9,10 @@ backupDir = "/home/ec2-user/environment/ictprg302-vetdss/backups"
 
 backupLog = "/home/ec2-user/environment/ictprg302-vetdss/backup.log"
 
+
 smtp = {"sender": "dcleary@sunitafe.edu.au",
         "recipient": "davidcgcleary@gmail.com",
         "server": "smtp.gmail.com",
         "port": 587,
         "user": "davidcgcleary@gmail.com", # need to specify a gmail email address with an app password setup
-        "password": "lemkllikuhmpdoog"}   # need a gmail app password     
+        "password": "hvzrncwveqvqjvty"}   # need a gmail app password     
